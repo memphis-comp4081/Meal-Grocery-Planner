2e547c632fa394aaef4e38d69de931c63a2068a6
kevinatown <kevinatown@gmail.com>
2015-11-10 23:16:55 -0600

    added bootstrap gem

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,8 @@
 class Ingredient < ActiveRecord::Base
 	has_many :pantries
+	
+	before_action :authenticate_
+
 	validates :description, presence: true
 	validates :unit_of_measure, presence: true
 end
