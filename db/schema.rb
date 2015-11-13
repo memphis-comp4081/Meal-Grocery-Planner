1b4bab6a69da89978faf5e7cf07a58c39e0dc805
kevinatown <kevinatown@gmail.com>
2015-11-13 13:23:00 -0600

    fixed merge db issue"

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110041814) do
+ActiveRecord::Schema.define(version: 20151113060218) do
 
   create_table "ingredients", force: :cascade do |t|
-    t.string   "description"
-    t.string   "unit_of_measure"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string "description," 
+    t.string "unit_of_measure"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "pantries", force: :cascade do |t|
