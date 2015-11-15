b576b10eee228479d1e8cb0c05b9505fa76eda67
kevinatown <kevinatown@gmail.com>
2015-11-14 19:46:21 -0600

    add new ingreident is up to date

--- app/controllers/ingredient_controller.rb
+++ app/controllers/ingredient_controller.rb
@@ -1,2 +1,23 @@
 class IngredientController < ApplicationController
+
+  def new
+  	@ingredient = Ingredient.new
+  end
+
+
+  def create
+    @ingredient = Ingredient.new(params_ingredient)
+    @ingredient.save! 
+  	redirect_to pantry_url
+  end
+
+  private
+
+  def params_ingredient
+	  params.require(:ingredient).permit(
+	    :description,
+	    :unit_of_measure
+  	)
+  end
+
 end
