4ff8f0717c65fbb6ef66ad5949ff42469cf2a171
kevinatown <kevinatown@gmail.com>
2015-11-16 15:20:33 -0600

    Created validations in Meal

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -4,6 +4,12 @@ class Meal < ActiveRecord::Base
 	belongs_to :menu
 	belongs_to :meal_lists
 
-	validates :name, presence: true
-	validates :description, presence: true
+	validates :name, presence: true, length: { 
+		within: 0..50,
+		wrong_length: "Must be between 0 and 50 characters." 
+	}
+	validates :description, presence: true, length: { 
+		within: 0..250,
+		wrong_length: "Must be between 0 and 250 characters." 
+	}
 end
