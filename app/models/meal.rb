dd8c6c3f6d24cc9cf0beb20bc9af1d9721f6d294
chhatten <hattencletus@gmail.com>
2015-11-17 05:29:38 +0000

    New Meal Page

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -4,6 +4,6 @@ class Meal < ActiveRecord::Base
 	belongs_to :menu
 	belongs_to :meal_lists
 
-	validates :name, presence: true
-	validates :description, presence: true
+	validates :name, presence: true, confirmation: true, length: {in: 0..50}, format: { with: /\A[a-zA-Z]+\z/, message: "Only letters"}
+	validates :description, presence: true, length: {in: 0..150}
 end
