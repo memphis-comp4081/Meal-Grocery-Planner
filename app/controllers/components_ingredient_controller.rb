4d32487eb15c83c6186c8ee0a68b99aa3129f092
jwood05 <jwood05@memphis.edu>
2015-11-16 23:26:26 -0600

    Finished Component and Components_Ingredient controllers/views

--- app/controllers/components_ingredient_controller.rb
+++ app/controllers/components_ingredient_controller.rb
@@ -1,7 +1,24 @@
 class ComponentsIngredientController < ApplicationController
+
+	#added to require login before pantry is shown.
+	before_action :authenticate_user!
+
   def add
+	@component = Component.find(params[:id])
+	@ingredients = Ingredient.all
+  end
+
+  def create
+	@comp_ingred = ComponentsIngredient.new(params.require(:components_ingredient).permit(:ingredient_id))
+	@comp_ingred.component_id = params[:id]
+	@comp_ingred.save!
+	redirect_to component_edit_url(params[:id])
   end
 
   def delete
+	#@comp_ingred = ComponentsIngredient.where(:id => params[:id])
+	#@component = Component.where(:id => @comp_ingred.component_id)
+	ComponentsIngredient.delete(params[:id])
+	redirect_to component_edit_url(params[:comp_id])
   end
 end
--- app/models/components_ingredient.rb
+++ app/models/components_ingredient.rb
@@ -1,2 +1,4 @@
 class ComponentsIngredient < ActiveRecord::Base
+	belongs_to :component
+	belongs_to :ingredient
 end
