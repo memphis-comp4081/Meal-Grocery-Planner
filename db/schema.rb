2b891d75039e858ec4ad741fc4477f0db47f6f7f
chhatten <hattencletus@gmail.com>
2015-12-06 07:53:54 +0000

    New Files

--- db/schema.rb
+++ db/schema.rb
@@ -56,9 +56,10 @@ ActiveRecord::Schema.define(version: 20151130154941) do
 
   create_table "meals", force: :cascade do |t|
     t.string   "name"
+    t.integer  "component_id"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "image"
   end
 
